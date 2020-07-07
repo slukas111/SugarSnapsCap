c9d1b8b23463edc2b7b86576e78663063bf876c2
Jonathan Jones <jonathan.jones51@yahoo.com>
2020-07-07 10:07:04 -0400

    Created a boxitem model as well as a categories model. Upadted the admin panel with the boxitem models and categories models

--- sugar_app/admin.py
+++ sugar_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import BoxItem, Categories
 
 # Register your models here.
+
+admin.site.register(BoxItem)
+admin.site.register(Categories)
--- sugar_app/models.py
+++ sugar_app/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Categories(models.Model):
+
+    CATEGORY_CHOICES = [
+        ('V', 'vegan'),
+        ('G', 'vegetarian'),
+        ('M', 'mixed'),
+    ]
+
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+
+    def __str__(self):
+        return self.category
+
+
+class BoxItem(models.Model):
+
+    Que_Choices = [
+        ('A', 'available'),
+        ('P', 'pending'),
+        ('C', 'claimed')
+    ]
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    expiration = models.DateTimeField()
+    que_assign = models.CharField(default='available', choices=Que_Choices, max_length=1)
+    image = models.ImageField(default="default.jpg", upload_to="boxitem")
+    profile = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
