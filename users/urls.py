20fe3ea7d655b4446f1544e63a9f3a4a8884c47d
Jonathan Jones <jonathan.jones51@yahoo.com>
2020-07-08 16:23:46 -0400

    Added a profile page that will display user data

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from users.views import index
+from users.views import index, user_profile_view
 
 urlpatterns = [
-    path('', index, name='homepage')
+    path('', index, name='homepage'),
+    path('profile/<int:user_id>', user_profile_view, name='user_profile')
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, reverse, HttpResponseRedirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
+
+from users.models import Profile
 # Create your views here.
 
 
@@ -11,4 +13,13 @@ def index(request):
     context = {
         'user': user
     }
+    return render(request, html, context)
+
+
+def user_profile_view(request, user_id):
+    html = 'user_profile.html'
+    profile = Profile.objects.get(id=user_id)
+    context = {
+        'user': profile.user
+        }
     return render(request, html, context)
