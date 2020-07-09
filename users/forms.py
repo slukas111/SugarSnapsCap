543439e4ce0cad950bcb168299b07b4812e6ed45
Jordan Davidson <jordandavidson744@yahoo.com>
2020-07-09 10:00:08 -0400

    added editUser page, updated user model to have a bio. Finished the edit user (for now).

--- users/forms.py
+++ users/forms.py
@@ -1,1 +1,11 @@
 from django import forms
+
+from users.models import Profile
+
+class EditProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = [
+            'profile_image',
+            'bio'
+            ]
