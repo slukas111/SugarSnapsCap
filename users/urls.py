8c123c2326193a09d364a158752a18db1548d12a
Sasha Lukas <sashamati@gmail.com>
2020-07-10 11:07:07 -0700

    added followers to view(unfollow/follow), had to update view for userprofileview. added conditional logic to template. follow/following button to profile page. extended base.html

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from users.views import index, user_profile_view, UserPostListView, EditUser
+from users.views import index, user_profile_view, UserPostListView, EditUser, Follow, Unfollow
 
 urlpatterns = [
     path('', index, name='homepage'),
     path('profile/<int:user_id>', user_profile_view, name='user_profile'),
     path('user/<str:username>/', UserPostListView.as_view(), name="userposts"),
-    path('edit/<int:id>', EditUser)
+    path('edit/<int:id>', EditUser),
+    path('follow/<int:id>/', Follow, name="follow"),
+    path('unfollow/<int:id>/', Unfollow, name="unfollow")
 
 ]
