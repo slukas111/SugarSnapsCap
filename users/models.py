4fc42880a21f4ac22573bc3ee88a8edea29e90cd
Binup karki <karki011@gmail.com>
2020-07-16 00:07:49 -0500

    user profile image update works

--- users/models.py
+++ users/models.py
@@ -13,7 +13,7 @@ class Profile(models.Model):
     profile_image = models.ImageField(default='default.jpg', upload_to='profile_pics')
     one_click_reserve = models.BooleanField(default=False)
     followers = models.ManyToManyField('self', symmetrical=False, blank=True, related_name='following')
-    bio = models.CharField(max_length=500, null=True)
+    bio = models.TextField( null=True)
     location = models.ForeignKey(Area, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
