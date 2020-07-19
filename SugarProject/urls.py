79f4cdc9df8cbcffab1c0488a191fb18194a6e91
Binup karki <karki011@gmail.com>
2020-07-19 15:36:11 -0500

    merge fix

--- SugarProject/urls.py
+++ SugarProject/urls.py
@@ -29,8 +29,8 @@ from sendemail.urls import urlpatterns as contacturls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('inbox/notifications/', include(notifications.urls, namespace='notifications')),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name="login"),
-    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name="logout"),
+    # path('login/', auth_views.LoginView.as_view(template_name='login.html'), name="login"),
+    # path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name="logout"),
 
 ]
 
