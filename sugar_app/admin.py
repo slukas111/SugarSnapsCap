d5b5a128f870b13a1055536ab76db856b7d286f4
Sasha Lukas <sashamati@gmail.com>
2020-07-22 05:49:26 -0700

    fixed followers inline, could not carousel

--- sugar_app/admin.py
+++ sugar_app/admin.py
@@ -3,5 +3,10 @@ from .models import BoxItem, Categories
 
 # Register your models here.
 
-admin.site.register(BoxItem)
+@admin.register(BoxItem)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'bio', 'location')
+    list_editable = ('location',)
+
+# admin.site.register(BoxItem)
 admin.site.register(Categories)
